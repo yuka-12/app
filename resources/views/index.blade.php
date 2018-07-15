d0246c33d7fcad0eb19e7729312117bcc0af846b
yuka <ykmrykmrykmrykmr@gmail.com>
2018-07-15 23:24:26 +0900

    トップページ作成、ルーティング

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+  <title>nijitter</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+  <div class='title'>
+    <h1>NIJITTER</h1>
+  </div>
+  <div class=''>
+    <form method="post">
+    <input type="text">
+    <input type="submit">
+  </form>
+</body>
+</html>
