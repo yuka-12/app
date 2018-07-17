1a67b82e2918d8f1abbf12e7a98a92fde5071448
yuka <ykmrykmrykmrykmr@gmail.com>
2018-07-17 22:48:30 +0900

    ビュー作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,10 +9,46 @@
   <div class='title'>
     <h1>NIJITTER</h1>
   </div>
-  <div class=''>
-    <form method="post">
-    <input type="text">
-    <input type="submit">
-  </form>
+  <div class='a'>
+    <form method="post" accept-charset="utf-8" return false>
+      <input type="text" id="name" name="name">
+      <textarea id="text" name="text"></textarea>
+    </form>
+
+    <input type="button" id="ajax" value="送信">
+  </div>
+    <div class="result"></div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script type="text/javascript">
+
+       $(function(){
+           $('#ajax').on('click',function(){
+               $.ajax({
+                   url:'./post',
+                   type:'POST',
+                   data:{
+                       'name':$('#name').val(),
+                       'text':$('#text').val()
+                   }
+               })
+               // Ajaxリクエストが成功した時発動
+               .done( (data) => {
+                   $('.result').html(data);
+                   console.log(data);
+               })
+               // Ajaxリクエストが失敗した時発動
+               .fail( (data) => {
+                   $('.result').html(data);
+                   console.log(data);
+               })
+               // Ajaxリクエストが成功・失敗どちらでも発動
+               .always( (data) => {
+
+               });
+           });
+       });
+
+   </script>
+
 </body>
 </html>
