acbb2c0b2b9a3be638870f92964e63e89ebab169
yuka <ykmrykmrykmrykmr@gmail.com>
2018-08-05 22:31:18 +0900

    見た目を整えた　jsで帰って来たjsonをhtmlに整えた

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,67 +3,95 @@
 <head>
   <title>nijitter</title>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/app.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <script src="bootstrap/js/bootstrap.min.js"></script>
   <script type="text/javascript">
 
+       function test(){
+         return $.ajax({
+           url: './get',
+           type: 'GET'
+         })
+       }
+           test().done(function(result) {
+             // console.log(result);
+             // HTMLを初期化
+             $(".posts").html("");
 
-      function test(){
-          return $.ajax({
-              url: './get',
-              type: 'GET'
-          })
-      }
-      test().done(function(result) {
-          $('.posts').html(result);
-      }).fail(function(result) {
-          console.log(result);
-      });
+             //HTMLを生成
+             // $.getJSON(result, function(data){
+             //   console.log(data);
+             // for(var index in result){
+             $(result).each(function(){
+             $(
+             '<h6>'+ this.name + '</h6>'+
+             '<span>' + this.text + '</span><br><br>'
+           ).appendTo('.posts');
+           });
+
+             // $('.posts').html(result);
+            }).fail(function(result) {
+                console.log(result);
+            });
 
 
 
-      $(function(){
-          $('#ajax').on('click',function(){
-              $.ajax({
-                  url:'./post',
-                  type:'POST',
-                  data:{
-                      'name':$('#name').val(),
-                      'text':$('#text').val()
-                  }
-              })
-              // Ajaxリクエストが成功した時発動
-                  .done( (data) => {
-                      $('.result').html(data);
-                      console.log(data);
-                  })
-                  // Ajaxリクエストが失敗した時発動
-                  .fail( (data) => {
-                      $('.result').html(data);
-                      console.log(data);
-                  })
-                  // Ajaxリクエストが成功・失敗どちらでも発動
-                  .always( (data) => {
 
-                  });
-          });
-      });
+       $(function(){
+           $('#ajax').on('click',function(){
+               $.ajax({
+                   url:'./post',
+                   type:'POST',
+                   data:{
+                       'name':$('#name').val(),
+                       'text':$('#text').val()
+                   }
+               })
+               // Ajaxリクエストが成功した時発動
+               .done( (data) => {
+                   $('.result').html(data);
+                   console.log(data);
+               })
+               // Ajaxリクエストが失敗した時発動
+               .fail( (data) => {
+                   $('.result').html(data);
+                   console.log(data);
+               })
+               // Ajaxリクエストが成功・失敗どちらでも発動
+               .always( (data) => {
 
-  </script>
+               });
+           });
+       });
+
+   </script>
 </head>
 <body>
-<div class='title'>
-  <h1>NIJITTER</h1>
-</div>
-<div class='form'>
-  <form id="form" method="post" accept-charset="utf-8" return false>
-    <input type="text" id="name" name="name"><br>
-    <textarea id="text" name="text"></textarea>
-  </form>
+  <div class='title m-3 p-3 text-center'>
+    <h1>NIJITTER</h1>
+  </div>
+  <div class="container center-block text-center">
+  <div class='form col-sm-5 center-block'>
+    <form id="form" method="post" accept-charset="utf-8" return false>
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" id="name" class="form-control" name="name"><br>
+    </div>
+    <div class="form-group">
+      <label for="text">Text:</label>
+      <textarea id="text" class="form-control" name="text"></textarea>
+    </div>
+    </form>
+    <input type="button" id="ajax" class="btn btn-primary" value="送信">
+  </div>
+  <br>
+  <br>
+  <div class="result"></div>
+  <div class="posts"></div>
 
-  <input type="button" id="ajax" value="送信">
 </div>
-<div class="result"></div>
-<div class="posts"></div>
 </body>
 </html>
