7b7533948b2436ceea64891c9a6cd3c5abaeac14
yuka <ykmrykmrykmrykmr@gmail.com>
2018-08-18 09:53:29 +0900

    get処理追加

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -9,53 +9,43 @@ use App\Post;
 
 class AppController extends Controller
 {
-    public function index()
-    {
-        return "hoge";
-    }
 
-    public function get()
-    {
-        return "hoge";
-    }
+  public function get()
+  {
+    $result = Post::all();
+    return $result->toArray();
 
-    public function post(Request $request)
-    {
-        if ($request['name'] && $request['text']) {
-            $name = $request['name'];
-            $text = $request['text'];
+  }
 
-            // $insert = DB::insert(
-            //   ['name' => $name], ['text' => $text]
-            // );
-            // $insert = DB::insert(
-            //   'insert into posts (name,text) values (?,?)',[$name,$text]
-            // );
+  public function post(Request $request)
+  {
+    if ($request['name'] && $request['text']) {
+    $name = $request['name'];
+    $text = $request['text'];
 
-            $post = new Post();
-            $post->name = $name;
-            $post->text = $text;
-            $post->save();
+    $post = new Post();
+    $post->name = $name;
+    $post->text = $text;
+    $post->save();
 
-            $str = "AJAX REQUEST SUCCESS\nname:".$name."\ntext:".$text."\n";
-            $result = nl2br($str);
-            return $result;
-        }else{
-            return 'FAIL TO AJAX REQUEST';
-        }
+    return 'success';
 
-    }
+  }else{
+    return 'FAIL TO AJAX REQUEST';
+  }
 
-    public function update()
-    {
-        //$companies = \DB::table('companies')->get();
-        //return view('index')->with('companies',$companies);
-        return "hoge";
-    }
-    public function delete()
-    {
-        //$companies = \DB::table('companies')->get();
-        //return view('index')->with('companies',$companies);
-        return "hoge";
-    }
+  }
+
+  public function update()
+  {
+      //$companies = \DB::table('companies')->get();
+      //return view('index')->with('companies',$companies);
+      return "hoge";
+  }
+  public function delete()
+  {
+      //$companies = \DB::table('companies')->get();
+      //return view('index')->with('companies',$companies);
+      return "hoge";
+  }
 }
